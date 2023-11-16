93f488d7c9251f2e9b106ef26220efcbfa12d14a
guibsonkrause <oliveira.guibson@hotmail.com>
2023-11-16 00:56:08 -0300

    criada tela inicial importacao

--- src/app/candidatos/candidato-import/candidato-import.component.ts
+++ src/app/candidatos/candidato-import/candidato-import.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CandidatoService } from '../shared/services/candidato.service';
 
 @Component({
   selector: 'app-candidato-import',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './candidato-import.component.html',
-  styleUrl: './candidato-import.component.css'
+  styleUrls: ['./candidato-import.component.css']
 })
 export class CandidatoImportComponent {
+  file: File | null = null;
+  resultado: any = null; // Substitua 'any' pelo tipo adequado conforme o DTO definido
 
+  constructor(private candidatoService: CandidatoService) {}
+
+  onFileSelected(event: any): void {
+    this.file = event.target.files[0];
+  }
+
+  onSubmit(): void {
+    if (this.file) {
+      this.candidatoService.importarCandidatos(this.file).subscribe(
+        (dto) => {
+          this.resultado = dto;
+        },
+        (error) => {
+          console.error('Erro ao enviar arquivo:', error);
+        }
+      );
+    }
+  }
 }
